3e86c03409c17d5be6eb5f69ccefe5ce5cd90678
Oleh Kulakevych <kor.geloer@gmail.com>
2023-02-20 20:21:31 +0200

    Initial Commit

---
+++ ios-pay-tag/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ios-pay-tag
+//
+//  Created by Oleh Kulakevych on 20.02.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
